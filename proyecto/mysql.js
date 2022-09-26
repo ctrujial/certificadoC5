11faae51f0ae81d94fbac7e8960a3ef81c15286b
ctrujial <cris92ta@gmail.com>
2022-09-26 05:37:28 -0500

    extructura para crud

---
+++ proyecto/mysql.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'usuarios',
+});
+
+conexion.connect((err) => {
+    if (err) throw err;{
+        console.log("Conectado correctamente");
+    }
+});
+
+const consultar = "SELECT * FROM USUARIO";
+conexion.query(consultar, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+});

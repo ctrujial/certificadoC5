6973bfac946fc09af5f14572792ee15c8e64db07
ctrujial <cris92ta@gmail.com>
2022-10-01 19:23:54 -0500

    01/10/2022

--- proyecto/App.js
+++ proyecto/App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {Text, View, StyleSheet, Image, TouchableOpacity, Alert} from "react-native"
+import imagenes from "./assets/buho.png";
+import * as ImagePicker from 'expo-image-picker';
+//importamos reac native para que sea androi
 
-export default function App() {
+const App = () => {
+// async sincronico
+  let utilizarImagePicker = async () => {
+    //primero llamo la biblioteca
+    let permisoDelUsuario = await ImagePicker.requestMediaLibraryPermissionsAsync();//esto pide permiso para ingresar a la galeria de imagenes
+
+    if (permisoDelUsuario.granted === false) {
+       alert("Se requiere utilizar la camara del celular");
+      return;
+    };
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>hola mundo!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <View style={styles.container}>
+    <Text style={styles.title}> hello world </Text>
+    <Image
+      source={imagenes}
+      style={styles.Image}
+      />
+    <TouchableOpacity 
+    onPress={utilizarImagePicker}
+    style={styles.button}
+    >
+     <Text style={styles.buttonTexto}>precioname</Text>
+    </TouchableOpacity>
+    
+  </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const styles =  StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems:"center", backgroundColor: "black"},
+  title: { fontSize: 30, color: "white"},
+  Image: {height: 150, width: 150, borderRadius: 90},
+  button: {backgroundColor: "chocolate", width: 180, alignItems: "center", borderRadius: 90, margin: 10},
+  buttonTexto: {fontSize: 20, fontWeight: "bold", fontStyle: "italic"}
 });
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
